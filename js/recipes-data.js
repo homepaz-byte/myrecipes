2313ee3794532ceb65631e80610b1d39723530fa
homepaz-byte <home.paz@gmail.com>
2026-09-16 15:42:03 +0300

    Update recipes-data.js

--- js/recipes-data.js
+++ js/recipes-data.js
@@ -45,4 +45,32 @@ const RECIPES = [
     url: "https://lizapanelim.com/%D7%A7%D7%A6%D7%99%D7%A6%D7%95%D7%AA-%D7%91%D7%A8%D7%95%D7%98%D7%91-%D7%90%D7%93%D7%95%D7%9D-%D7%A2%D7%9D-%D7%A4%D7%99%D7%A8%D7%94/",
     photoUrl: "https://lizapanelim.com/wp-content/uploads/2013/06/0fe43zy1hveygd9x6qsf.jpg.webp",
   },
+  {
+    name: "עוגת קפה ושוקולד",
+    category: "עוגות בחושות",
+    sourceLabel: "adikosh.co.il",
+    url: "https://adikosh.co.il/pound-cake/%D7%A2%D7%95%D7%92%D7%AA-%D7%A7%D7%A4%D7%94-%D7%95%D7%A9%D7%95%D7%A7%D7%95%D7%9C%D7%93/",
+    photoUrl: "https://adikosh.co.il/wp-content/uploads/2020/03/80897334_10214716999711317_5798059077682593792_n-1-e1584807202103.jpg",
+  },
+  {
+    name: "עוגת גזר וקרם גבינת שמנת",
+    category: "עוגות בחושות",
+    sourceLabel: "naamagaon.com",
+    url: "https://www.naamagaon.com/%D7%9B%D7%9C-%D7%94%D7%9E%D7%AA%D7%9B%D7%95%D7%A0%D7%99%D7%9D/%D7%A2%D7%95%D7%92%D7%AA-%D7%92%D7%96%D7%A8-%D7%95%D7%A7%D7%A8%D7%9D-%D7%92%D7%91%D7%99%D7%A0%D7%AA-%D7%A9%D7%9E%D7%A0%D7%AA/",
+    photoUrl: "https://www.naamagaon.com/wp-content/uploads/2024/01/%D7%91%D7%A9%D7%A8%D7%A8%D7%9D%D7%90-scaled.jpg",
+  },
+  {
+    name: "מאפינס אוכמניות וקראמבל",
+    category: "עוגות בחושות",
+    sourceLabel: "naamagaon.com",
+    url: "https://www.naamagaon.com/%D7%9B%D7%9C-%D7%94%D7%9E%D7%AA%D7%9B%D7%95%D7%A0%D7%99%D7%9D/%D7%9E%D7%90%D7%A4%D7%99%D7%A0%D7%A1-%D7%90%D7%95%D7%9B%D7%9E%D7%A0%D7%99%D7%95%D7%AA-%D7%95%D7%A7%D7%A8%D7%90%D7%9E%D7%91%D7%9C/",
+    photoUrl: "https://www.naamagaon.com/wp-content/uploads/2023/02/IMG_3445-scaled.jpg",
+  },
+  {
+    name: "עוגת יוגורט לימון",
+    category: "עוגות בחושות",
+    sourceLabel: "lizapanelim.com",
+    url: "https://lizapanelim.com/%D7%A2%D7%95%D7%92%D7%AA-%D7%99%D7%95%D7%92%D7%95%D7%A8%D7%98-%D7%9C%D7%99%D7%9E%D7%95%D7%9F/",
+    photoUrl: "https://lizapanelim.com/wp-content/uploads/2020/07/DSCF6119.jpg.webp",
+  },
 ];
